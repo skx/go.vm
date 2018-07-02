d0e8318db4a103276e8281e2cb617fe35c7fdf7b
Steve Kemp <steve@steve.org.uk>
2018-07-02 12:39:31 +0300

    Reimplement the register-object using objects.
    Rather than manually storing types in a register, and choosing the
    correct field, we now store "Objects".  There are two concrete object
    types at the moment - string + integer, just like before.
    
    This gives us flexibility in the future if we wished to store
    doubles, arrays, etc.

--- cpu/register.go
+++ cpu/register.go
@@ -8,23 +8,37 @@ import (
 	"os"
 )
 
-// Register holds the contents of a single register.
+// Object is the interface for something we store in a register.
 //
-// This is horrid because we don't use an enum for the type.
-type Register struct {
-	// Integer contents of register if t == "int"
-	i int
-
-	// String contents of register if t == "string"
-	s string
-
-	// Register type: "int" vs. "string"
-	t string
+// This is done to allow future expansion, and also for neatness.
+type Object interface {
+	Type() string
 }
 
+// IntegerObject is an object holding an integer-value.
+type IntegerObject struct {
+	Value int
+}
+
+// Type returns `int` for IntegerObjects.
+func (i *IntegerObject) Type() string { return "int" }
+
+// StringObject is an object holding a string-value.
+type StringObject struct {
+	Value string
+}
+
+// Type returns `string` for StringObjects.
+func (i *StringObject) Type() string { return "string" }
+
+// Register holds the contents of a single register, as an object.
 //
-// Register functions
-//
+// This means it can hold either an IntegerObject, or a StringObject.
+type Register struct {
+	o Object
+}
+
+// NewRegister is the constructor for a register.
 func NewRegister() *Register {
 	r := &Register{}
 	r.SetInt(0)
@@ -32,38 +46,45 @@ func NewRegister() *Register {
 }
 
 // GetInt retrieves the integer content of the given register.
-// If the register contains a string that is a fatal error.
+// If the register does not contain an integer that is a fatal error.
 func (r *Register) GetInt() int {
-	if r.t != "int" {
-		fmt.Printf("BUG: Attempting to call GetInt on a register holding a non-integer value.\n")
+	switch arg := r.o.(type) {
+	case *IntegerObject:
+		return arg.Value
+	default:
+		fmt.Printf("BUG: Attempting to call GetInt on a register holding a non-integer value: %v\n", r.o)
 		os.Exit(3)
 	}
-	return r.i
+	// Not reached
+	return 0
 }
 
 // SetInt stores the given integer in the register.
 func (r *Register) SetInt(v int) {
-	r.i = v
-	r.t = "int"
+	r.o = &IntegerObject{Value: v}
 }
 
 // GetInt retrieves the string content of the given register.
-// If the register contains an integer that is a fatal error.
+// If the register does not contain a string that is a fatal error.
 func (r *Register) GetString() string {
-	if r.t != "string" {
-		fmt.Printf("BUG: Attempting to call GetString on a register holding a non-string value.\n")
+	switch arg := r.o.(type) {
+	case *StringObject:
+		return arg.Value
+	default:
+		fmt.Printf("BUG: Attempting to call GetString on a register holding a non-string value: %v\n", r.o)
 		os.Exit(3)
 	}
-	return r.s
+
+	// Not reached
+	return ""
 }
 
 // SetString stores the supplied string in the register.
 func (r *Register) SetString(v string) {
-	r.s = v
-	r.t = "string"
+	r.o = &StringObject{Value: v}
 }
 
 // Return the type of a registers contents `int` vs. `string`.
 func (r *Register) Type() string {
-	return (r.t)
+	return (r.o.Type())
 }
