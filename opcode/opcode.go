f093b4c0d821528f55c6de809735d6c1d48be4a5
Steve Kemp <steve@steve.org.uk>
2018-07-27 12:29:25 +0300

    Removed whitespace

--- opcode/opcode.go
+++ opcode/opcode.go
@@ -3,52 +3,43 @@ package opcode
 var (
 	// EXIT is our first opcode.
 	// The rest follow, grouped by functionality.
-	EXIT = 0x00
-
-	INT_STORE    = 0x01
-	INT_PRINT    = 0x02
-	INT_TOSTRING = 0x03
-	INT_RANDOM   = 0x04
-
-	JUMP_TO = 0x10
-	JUMP_Z  = 0x11
-	JUMP_NZ = 0x12
-
-	XOR_OP = 0x20
-	ADD_OP = 0x21
-	SUB_OP = 0x22
-	MUL_OP = 0x23
-	DIV_OP = 0x24
-	INC_OP = 0x25
-	DEC_OP = 0x26
-	AND_OP = 0x27
-	OR_OP  = 0x28
-
+	EXIT          = 0x00
+	INT_STORE     = 0x01
+	INT_PRINT     = 0x02
+	INT_TOSTRING  = 0x03
+	INT_RANDOM    = 0x04
+	JUMP_TO       = 0x10
+	JUMP_Z        = 0x11
+	JUMP_NZ       = 0x12
+	XOR_OP        = 0x20
+	ADD_OP        = 0x21
+	SUB_OP        = 0x22
+	MUL_OP        = 0x23
+	DIV_OP        = 0x24
+	INC_OP        = 0x25
+	DEC_OP        = 0x26
+	AND_OP        = 0x27
+	OR_OP         = 0x28
 	STRING_STORE  = 0x30
 	STRING_PRINT  = 0x31
 	STRING_CONCAT = 0x32
 	STRING_SYSTEM = 0x33
 	STRING_TOINT  = 0x34
-
 	CMP_REG       = 0x40
 	CMP_IMMEDIATE = 0x41
 	CMP_STRING    = 0x42
 	IS_STRING     = 0x43
 	IS_INTEGER    = 0x44
-
-	NOP_OP    = 0x50
-	REG_STORE = 0x51
-
-	PEEK   = 0x60
-	POKE   = 0x61
-	MEMCPY = 0x62
-
-	STACK_PUSH = 0x70
-	STACK_POP  = 0x71
-	STACK_RET  = 0x72
-	STACK_CALL = 0x73
-
-	TRAP_OP = 0x80
+	NOP_OP        = 0x50
+	REG_STORE     = 0x51
+	PEEK          = 0x60
+	POKE          = 0x61
+	MEMCPY        = 0x62
+	STACK_PUSH    = 0x70
+	STACK_POP     = 0x71
+	STACK_RET     = 0x72
+	STACK_CALL    = 0x73
+	TRAP_OP       = 0x80
 )
 
 // Opcode is a holder for a single instruction.
