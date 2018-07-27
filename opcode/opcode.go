757078bf2d90ec841e2a0a3fe0275846ef16c9e3
Steve Kemp <steve@steve.org.uk>
2018-07-27 12:27:02 +0300

    Fixed comments on functions

--- opcode/opcode.go
+++ opcode/opcode.go
@@ -1,22 +1,16 @@
 package opcode
 
 var (
-	// Exit
 	EXIT = 0x00
-
-	// Int operations
 
 	INT_STORE    = 0x01
 	INT_PRINT    = 0x02
 	INT_TOSTRING = 0x03
 	INT_RANDOM   = 0x04
 
-	//  Jumps
 	JUMP_TO = 0x10
 	JUMP_Z  = 0x11
 	JUMP_NZ = 0x12
-
-	//  Mathematical
 
 	XOR_OP = 0x20
 	ADD_OP = 0x21
@@ -28,14 +22,11 @@ var (
 	AND_OP = 0x27
 	OR_OP  = 0x28
 
-	//  String operations
 	STRING_STORE  = 0x30
 	STRING_PRINT  = 0x31
 	STRING_CONCAT = 0x32
 	STRING_SYSTEM = 0x33
 	STRING_TOINT  = 0x34
-
-	//  Comparison functions
 
 	CMP_REG       = 0x40
 	CMP_IMMEDIATE = 0x41
@@ -43,25 +34,18 @@ var (
 	IS_STRING     = 0x43
 	IS_INTEGER    = 0x44
 
-	//  Misc things
-
 	NOP_OP    = 0x50
 	REG_STORE = 0x51
-
-	//  Load from RAM/store in RAM
 
 	PEEK   = 0x60
 	POKE   = 0x61
 	MEMCPY = 0x62
-
-	//  Stack operations
 
 	STACK_PUSH = 0x70
 	STACK_POP  = 0x71
 	STACK_RET  = 0x72
 	STACK_CALL = 0x73
 
-	// Interrupt / trap
 	TRAP_OP = 0x80
 )
 
